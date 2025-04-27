ac0019b48daf2a32558c81077a6136cafeb7c253
aa775319114 <satihayashi1@gmail.com>
2025-04-27 17:29:44 +0300

    1

--- database.js
+++ database.js
@@ -1,23 +1,22 @@
 const fs = require("fs");
-const { userInfo } = require("os");
-
+// Шлях до файлу, у якому буде наша база даних
 const dbFile = "./chat.db";
 const exists = fs.existsSync(dbFile);
 const sqlite3 = require("sqlite3").verbose();
-const dbWrapper =require("sqlite");
+const dbWrapper = require("sqlite");
 let db;
-
 dbWrapper
-    .open({
-        filename: dbFile,
-        driver: sqlite3.Database
-    })
-    .then(async dBase => {
-
-db = dBase;
-try {
-    if(!exists) {
-
+  .open({
+    filename: dbFile,
+    driver: sqlite3.Database
+  })
+  .then(async dBase => {
+    db = dBase;
+    // Використвуємо try-catch у разі якщо виникнуть помилки
+    try {
+      // Перевіряємо чи існує уже файл бази даних
+      if (!exists) {
+        // Якщо не існує то створюємо таблиці
         await db.run(
             `CREATE TABLE user(
                 user_id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -26,41 +25,42 @@ try {
             );`
         );
         await db.run(
-            `INSERT INTO user (login, password) VALUES
-            ('admin', 'admin'),
-            ('JavaScript', 'banana'),
-            ('user1', 'password1');`
+          `INSERT INTO user (login, password) VALUES
+          ('admin', 'admin'),
+          ('JavaScript', 'banana'),
+          ('user1', 'password1');`
         );
         await db.run(
             `CREATE TABLE message(
                 msg_id INTEGER PRIMARY KEY AUTOINCREMENT,
                 content TEXT,
                 autor INTEGER,
-                FOREIGHT KEY (autor) REFERENCES user(user_id)
+                FOREIGN KEY(autor) REFERENCES user(user_id)
             );`
-        );
-    } else {
-    console.log (await db.all("SELECT * from user"));
+        );s
+      } else {
+        console.log(await db.all("SELECT * from user"));
+      }
+    } catch (dbError) {
+      console.error(dbError);
     }
-} catch (dbError) {
-    console.error(dbError);
-}
-})
+  });
 module.exports = {
-    getMessage: async () =>{
-        try {
-            return await db.all(
-                `SELECT msg_id, content, login, user_id from message
-                JOIN user ON message.autor = user.user_id`
-            );
-        } catch (dbError) {
-            console.error(dbError);
-        }
-    },
-    addMessage: async(msg, userId) => {
-        await db.run(
-            `INSERT INTO message (content, autor) VALUES (?, ?)`,
-            [msg, userId]
+  getMessages: async () => {
+    try {
+      return await db.all(
+        `SELECT msg_id, content, login, user_id from message
+         JOIN user ON message.autor = user.user_id`
         );
+    } catch (dbError) {
+      console.error(dbError);
     }
+  },
+  addMessage: async (msg, userId) => {
+    await db.run(
+      `INSERT INTO message (content, autor) VALUES (?, ?)`,
+      [msg, userId]
+    );
+  }
 };
+
