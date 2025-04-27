e247ddfe6d5393ce3cf272c9db38b69ac76d6ec8
aa775319114 <satihayashi1@gmail.com>
2025-04-27 16:33:12 +0300

    1

--- database.js
+++ database.js
@@ -5,3 +5,21 @@ const exists = fs.existsSync(dbFile);
 const sqlite3 = require("sqlite3").verbose();
 const dbWrapper =require("sqlite");
 let db;
+
+db = dBase;
+try {
+    if(!exists) {
+
+        await db.run(
+            `CREATE TABLE user(
+                user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                login TEXT,
+                password TEXT
+            );`
+        );
+    } else {
+    console.log (await db.all("SELECT * from user"));
+    }
+} catch (dbError) {
+    console.error(dbError);
+}
